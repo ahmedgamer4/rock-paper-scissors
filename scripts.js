cf096fd76d69b50b737165c586f885342ee5756b
Your Name <yourname@example.com>
2022-08-14 08:19:52 -0400

    Added the logic

--- scripts.js
+++ scripts.js
@@ -9,13 +9,38 @@ function playRound(){
     let ply_score = 0;
     let buttons = document.querySelectorAll('button');
     let button_name = "";
-    buttons.forEach(button => {
-        button.addEventListener('click', e => {
-            button_name = e.target.id;
-        });
-    });
+    for (let i = 0; i < 5; i++){
+        buttons.forEach(button => {
+            button.addEventListener('click', e => {
+                button_name = e.target.id;
+            
+                if (button_name === getComputerChoice()){
+                    return 'tie';
+                }
+                else if (button_name === "paper" && getComputerChoice() === "rock"){
+                    ply_score++;
+                }
+                else if (button_name === 'scissors' && getComputerChoice() === "paper"){
+                    ply_score++;
+                }
+                else if (button_name === "rock" && getComputerChoice() === "scissors"){
+                    ply_score++;
+                }
+                else if (button_name === "rock" && getComputerChoice() === 'paper'){
+                    cmp_score++;
+                }
+                else if (button_name === "scissors" && getComputerChoice() === 'rock'){
+                    cmp_score++;
+                }
+                else{
+                    cmp_score++;
+                }
 
-    
+                console.log(cmp_score);
+                console.log(ply_score);
+            });
+        });
+    }
 }
 
 
