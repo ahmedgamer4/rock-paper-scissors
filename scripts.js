45487eb43bb8e01a0ada84d88b9ad6de4ee7fa53
Your Name <yourname@example.com>
2022-08-14 09:06:06 -0400

    Fixed the curren Logic; Added some boxes

--- scripts.js
+++ scripts.js
@@ -9,38 +9,53 @@ function playRound(){
     let ply_score = 0;
     let buttons = document.querySelectorAll('button');
     let button_name = "";
-    for (let i = 0; i < 5; i++){
-        buttons.forEach(button => {
-            button.addEventListener('click', e => {
-                button_name = e.target.id;
-            
-                if (button_name === getComputerChoice()){
-                    return 'tie';
-                }
-                else if (button_name === "paper" && getComputerChoice() === "rock"){
-                    ply_score++;
-                }
-                else if (button_name === 'scissors' && getComputerChoice() === "paper"){
-                    ply_score++;
-                }
-                else if (button_name === "rock" && getComputerChoice() === "scissors"){
-                    ply_score++;
-                }
-                else if (button_name === "rock" && getComputerChoice() === 'paper'){
-                    cmp_score++;
-                }
-                else if (button_name === "scissors" && getComputerChoice() === 'rock'){
-                    cmp_score++;
+    let final_r = document.getElementById('final_r');
+    
+    buttons.forEach(button => {
+        button.addEventListener('click', e => {
+            button_name = e.target.id;
+            let ply = document.getElementById('ply_score');
+            let cmp = document.getElementById('cmp_score');
+            ply.textContent = ply_score;
+            cmp.textContent = cmp_score;
+             
+            while (ply_score + cmp_score === 5){
+                if (ply_score === 3){
+                    final_r.textContent = 'You Win!';
+                    return 1;
                 }
                 else{
-                    cmp_score++;
+                    final_r.textContent = 'You Lose!';
+                    return 1;
                 }
+            } 
+            
+            if (button_name === getComputerChoice()){
+                return 'tie';
+            }
+            else if (button_name === "paper" && getComputerChoice() === "rock"){
+                ply_score++;
+            }
+            else if (button_name === 'scissors' && getComputerChoice() === "paper"){
+                ply_score++;
+            }
+            else if (button_name === "rock" && getComputerChoice() === "scissors"){
+                ply_score++;
+            }
+            else if (button_name === "rock" && getComputerChoice() === 'paper'){
+                cmp_score++;
+            }
+            else if (button_name === "scissors" && getComputerChoice() === 'rock'){
+                cmp_score++;
+            }
+            else{
+                cmp_score++;
+            }
 
-                console.log(cmp_score);
-                console.log(ply_score);
-            });
+            console.log(cmp_score);
+            console.log(ply_score);
         });
-    }
+    });
 }
 
 
@@ -56,5 +71,6 @@ function playRound(){
     playerSelection, computerSelection
 }
 game()*/
+
 
 playRound()
